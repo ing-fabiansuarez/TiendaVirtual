3fed074e55b47bfd77a17aa869b0a3031eafc452
Fabian Suarez <inge.fabiansuarez@gmail.com>
2024-03-06 11:28:55 -0500

    Initial commit

---
+++ app/src/main/java/com/fabiansuarez/tiendavirtual/Product.java
@@ -0,0 +1,50 @@
+package com.fabiansuarez.tiendavirtual;
+
+public class Product {
+    private String name;
+    private String descripcion;
+    private Double price;
+    private String urlImage;
+
+    public Product() {
+    }
+
+    public Product(String name, String descripcion, Double price, String urlImage) {
+        this.name = name;
+        this.descripcion = descripcion;
+        this.price = price;
+        this.urlImage = urlImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
